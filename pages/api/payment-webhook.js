9c89a4aa907087e37a73ce4c7635f991a39974d8
itzme-challa <challawaheed4@gmail.com>
2025-05-16 10:59:55 +0530

    Update payment-webhook.js

--- pages/api/payment-webhook.js
+++ pages/api/payment-webhook.js
@@ -1,4 +1,5 @@
-const { Cashfree } = require('@cashfreepayments/cashfree-pg');
+const { Cashfree } = require('cashfree-pg-node-sdk');
+
 export default async function handler(req, res) {
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method not allowed' });
